0d8897447b91f5e92dd1561a9b61cf6a1fcecd8e
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-24 17:35:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,9 @@ def line(array)
   if array.length < 1
     puts "The line is currently empty."
   elsif array > 0
-    
+    new_arr = []
+    counter = 0
+    while counter < array.length
+      new_el = "#{counter+1}. #{array[counter]}"
   end
 end
